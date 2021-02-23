9bf0dca9ec754e60780c113c4b787dda311671bc
Latifur Rahman <latifurrahman350@gmail.com>
2021-02-23 21:04:05 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result = add (12, 82);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+const result = add (12, 82);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+    return;
+}
+
+const result = add (12);
+console.log(result);
+
+const premik = {name: "alia", phone: 45124754};
+console.log(premik.gf);
+
+let bandorTypeDeveloper = undefined;
+console.log(bandorTypeDeveloper);
---
+++ true-false.js
@@ -0,0 +1,23 @@
+const age = 0;
+if(age){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+
+
+const name = "";
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+
+
+// false ki ki
+// 0
+//""
+// undefined
+// null
+// NaN
+// false
